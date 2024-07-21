a6ce681b501161d0aaef05b0f50754a2ca312cee
cjs1301 <cjs5241@gmail.com>
2024-07-22 02:14:08 +0900

    add GA

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -34,7 +34,7 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-gtag`,
       options: {
-        trackingId: `G-XXXXXXXX`,
+        trackingId: `GTM-WWZ2WZLG`,
         head: true,
       },
     },
